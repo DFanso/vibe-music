999c8e9b1524875f4761c7efc2935667e8df8ef8
Leo Gavin <71537398+DFanso@users.noreply.github.com>
2025-03-28 13:44:43 +0530

    hepa mention

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -8,7 +8,7 @@ module.exports = {
   async execute(interaction: CommandInteraction) {
 
     try {
-      await interaction.reply(`Hello, ${interaction.user}!, I'm Hemaka, aka @hephyrian. I was replaced by AI because I can't come to Discord very often—I'm working and don't have a stable connection. So, my friends replaced me with a Discord bot. Thanks 🍒 (Also, I like Black girls and BDSM.)`);
+      await interaction.reply(`Hello, ${interaction.user}!, I'm Hemaka, aka <@968542632844349471>. I was replaced by AI because I can't come to Discord very often—I'm working and don't have a stable connection. So, my friends replaced me with a Discord bot. Thanks 🍒 (Also, I like Black girls and BDSM.)`);
     } catch (error) {
       console.error(error);
       await interaction.reply('There was an error executing the command.');
